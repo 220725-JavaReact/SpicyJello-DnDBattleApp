8c0d12bccef1ff7cd113dc358ca65c9c2b4efaa9
Warren Lee <wplee327@users.noreply.github.com>
2022-09-13 15:22:22 -0400

    Add PUT and DELETE mappings to UserController; Use PasswordFactory to encode password strings when adding and updating users

--- src/main/java/com/spicyjello/dndbattleappbe/controller/UserController.java
+++ src/main/java/com/spicyjello/dndbattleappbe/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.spicyjello.dndbattleappbe.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +28,33 @@ public class UserController {
 	@GetMapping
 	public List<User> getAllUsers() {
 		return userService.getAllUsers();
+	}
+	@GetMapping("/id/{id}")
+	public User getUserById(@PathVariable String id) {
+		Optional<User> foundUser = userService.getUserById(Integer.parseInt(id));
+		if (foundUser.isPresent()) return foundUser.get();
+		else {
+			User notFoundUser = new User();
+			notFoundUser.setName("User not found");
+			return notFoundUser;
+		}
+	}
+	@GetMapping("/username/{username}")
+	public User getUserByUsername(@PathVariable String username) {
+		Optional<User> foundUser = userService.getUserByUsername(username);
+		if (foundUser.isPresent()) return foundUser.get();
+		else {
+			User notFoundUser = new User();
+			notFoundUser.setName("User not found");
+			return notFoundUser;
+		}
+	}
+	@PutMapping
+	public User updateUser(@RequestBody User updatedUser) {
+		return userService.updateUser(updatedUser);
+	}
+	@DeleteMapping("/id/{id}")
+	public Boolean deleteUserById(@PathVariable String id) {
+		return userService.deleteUserById(Integer.parseInt(id));
 	}
 }
--- src/main/java/com/spicyjello/dndbattleappbe/service/UserService.java
+++ src/main/java/com/spicyjello/dndbattleappbe/service/UserService.java
@@ -8,12 +8,16 @@ import org.springframework.stereotype.Service;
 
 import com.spicyjello.dndbattleappbe.data.UserRepository;
 import com.spicyjello.dndbattleappbe.model.User;
+import com.spicyjello.dndbattleappbe.util.PasswordFactory;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository userRepo;
+	@Autowired
+	private PasswordFactory passwordFactory;
 	public User addUser(User newUser) {
+		newUser.setPassword(passwordFactory.encodePassword(newUser.getPassword()));
 		return userRepo.save(newUser);
 	}
 	public List<User> getAllUsers() {
@@ -21,6 +25,20 @@ public class UserService {
 	}
 	public Optional<User> getUserById(int id) {
 		return userRepo.findById(id);
+	}
+	public Optional<User> getUserByUsername(String username) {
+		return userRepo.findByUsername(username);
+	}
+	public User updateUser(User updatedUser) {
+		Optional<User> foundUser = getUserById(updatedUser.getId());
+		if (foundUser.isPresent()) {
+			if (!updatedUser.getPassword().equals(foundUser.get().getPassword())) updatedUser.setPassword(passwordFactory.encodePassword(updatedUser.getPassword()));
+			return userRepo.save(updatedUser);
+		} else {
+			User notUpdatedUser = new User();
+			notUpdatedUser.setName("User not updated");
+			return notUpdatedUser;
+		}
 	}
 	public Boolean deleteUserById(int id) {
 		userRepo.deleteById(id);

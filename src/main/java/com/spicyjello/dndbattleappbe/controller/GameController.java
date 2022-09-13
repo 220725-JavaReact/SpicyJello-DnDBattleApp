2dbd4ea827cd639af6050503fb515a53f1f4199a
Warren Lee <wplee327@users.noreply.github.com>
2022-09-13 18:45:28 -0400

    Combine getting user by id and username and improve url mapping; Finish endpoints for GameController

--- src/main/java/com/spicyjello/dndbattleappbe/controller/GameController.java
+++ src/main/java/com/spicyjello/dndbattleappbe/controller/GameController.java
@@ -1,10 +1,14 @@
 package com.spicyjello.dndbattleappbe.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +28,23 @@ public class GameController {
 	@GetMapping
 	public List<Game> getAllGames() {
 		return gameService.getAllGames();
+	}
+	@GetMapping("/user/{id}")
+	public List<Game> getAllGamesByUserId(@PathVariable String id) {
+		return gameService.getAllGamesByUserId(Integer.parseInt(id));
+	}
+	@GetMapping("{id}")
+	public Game getGameById(@PathVariable String id) {
+		Optional<Game> foundGame = gameService.getGameById(Integer.parseInt(id));
+		if (foundGame.isPresent()) return foundGame.get();
+		else return new Game();
+	}
+	@PutMapping
+	public Game updateGame(@RequestBody Game updatedGame) {
+		return gameService.updateGame(updatedGame);
+	}
+	@DeleteMapping("{id}")
+	public Boolean deleteGameById(@PathVariable String id) {
+		return gameService.deleteGameById(Integer.parseInt(id));
 	}
 }

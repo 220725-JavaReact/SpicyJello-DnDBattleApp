aa115929791de804c9b30538f6fd9b063ec42f55
Warren Lee <wplee327@users.noreply.github.com>
2022-09-15 14:10:06 -0400

    Add CORS configuration class

--- src/main/java/com/spicyjello/dndbattleappbe/util/WebSecurityConfig.java
+++ src/main/java/com/spicyjello/dndbattleappbe/util/WebSecurityConfig.java
@@ -8,10 +8,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
 		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
 	}
-
 }

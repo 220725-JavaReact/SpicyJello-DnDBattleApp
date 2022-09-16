74ec2b8ec556b14913a47e82537bd108d89dca7a
Warren Lee <wplee327@users.noreply.github.com>
2022-09-16 18:08:12 -0400

    Fix typo on folder name; fix cors settings

--- dndbattleappbe/src/main/java/com/spicyjello/dndbattleappbe/util/WebSecurityConfig.java
+++ dndbattleappbe/src/main/java/com/spicyjello/dndbattleappbe/util/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors();
 		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
 	}
 }
